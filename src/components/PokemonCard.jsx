5d51d7018450529f216e3dbd03f285d9e80db05d
ornel77 <t.ornel@yahoo.fr>
2023-03-26 10:04:45 +0200

    add condition and an object

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,25 @@
 import pokemonImg from './pokemon.png';
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 
 function PokemonCard() {
+    const pokemon = pokemonList[0]
     return (
-        <figure>
-            <img src={pokemonImg} alt="bulbazor"/>
-            <figcaption>Bulbazor</figcaption>
-        </figure>
+        pokemon.imgSrc ? (
+            <figure>
+                <img src={pokemon.imgSrc} alt={pokemon.name}/>
+                <figcaption>{pokemon.name}</figcaption>
+            </figure>
+        ) : <p>???</p>
     )
 }
 
