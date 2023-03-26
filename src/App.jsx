57704b7e51cae48a31fd75d243ddedb878565918
ornel77 <t.ornel@yahoo.fr>
2023-03-26 13:05:15 +0200

    add button and a list of pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 import './App.css'
+import { useState } from 'react';
+
 import  PokemonCard  from './components/PokemonCard'
 
 const pokemonList = [
   {
       name: "bulbasaur",
       imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
-  },
-];
+    },
+  ];
 
 function App() {
-
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemonList={pokemonList}/>
     </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react';
 
-function PokemonCard({ pokemon }) {
+function PokemonCard({ pokemonList }) {
+    const [ pokemonIndex, setPokemonIndex ] = useState(0)
+
+    const handlePrecedingClick = () => {
+        setPokemonIndex(pokemonIndex - 1)
+    }
+
+    const handleNextClick = () => {
+        setPokemonIndex(pokemonIndex + 1)
+    }
+
+    // const [ pokemon ]= pokemonList
+    // console.log(pokemonList[pokemonIndex].imgSrc)
+
     return (
         (
             <figure>
                 {
-                    pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> 
+                    pokemonList[pokemonIndex].imgSrc ? <img src={pokemonList[pokemonIndex].imgSrc} alt={pokemonList[pokemonIndex].name}/> 
                     :
                     <p>???</p>
                 }
+                <button className="precedent" onClick={handlePrecedingClick} >Précedent</button>
+                <button className="next" onClick={handleNextClick} >Suivant</button>
             </figure>
         ) 
     )
