45ee9d899a5180fc5ee4fcb4639c119c14d210cf
ornel77 <t.ornel@yahoo.fr>
2023-03-30 12:00:32 +0200

    fix bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,22 @@
 function NavBar({pokemonIndex, setPokemonIndex, pokemonList}) {
 
     // console.log(pokemonList.indexOf(pokemonList.filter(pokemon => pokemon.name == "charmander")))
-    // const handlePrecedingClick = () => {
-    //     setPokemonIndex(pokemonIndex - 1)
-    // }
+    const handlePrecedingClick = () => {
+        setPokemonIndex(pokemonIndex - 1)
+    }
 
-    // const handleNextClick = () => {
-    //     setPokemonIndex(pokemonIndex + 1)
-    // }
+    const handleNextClick = () => {
+        setPokemonIndex(pokemonIndex + 1)
+    }
     return (
+        
         <div>
-            {pokemonList.map(pokemon => (
-            <button key={pokemon.name}>{pokemon.name}</button>
-            ))}
+            {
+                pokemonIndex > 0 && <button onClick={handlePrecedingClick} >Précedent</button>
+            }
+            {
+                pokemonIndex < pokemonList.length - 1 && <button onClick={handleNextClick} >Suivant</button>
+            }
         </div>
         
     )
